a9da74a16140ed8b7d5bb58d11dbedacc8c729cd
Devin Yin <117371221+DetonateYin2077@users.noreply.github.com>
2024-10-17 19:28:55 +0800

    Add files via upload

---
+++ out/_next/static/3R2t_QodklD1W4V5ryESm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-590900a8d7efcbc6.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
